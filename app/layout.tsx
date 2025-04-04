c8477c509cc9ed6174e06c0238ecfefa0b667faf
mackey55555 <a.makihara@auris.jp>
2025-04-04 18:08:46 +0900

    Adsense追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-3556606235552037" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3556606235552037"
